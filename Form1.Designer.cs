107ca593aaf57251e48c97fb11a29c4239cec706
Bogdan <bogdandjakovic123@gmail.com>
2023-09-02 19:32:01 +0200

    'init'

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Locker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.open = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.AutoSize = true;
+            this.open.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open.ForeColor = System.Drawing.Color.Crimson;
+            this.open.Location = new System.Drawing.Point(179, 9);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(81, 13);
+            this.open.TabIndex = 0;
+            this.open.Text = "LeagueClient";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.errorLabel);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(471, 288);
+            this.panel1.TabIndex = 1;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(12, 265);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(10, 13);
+            this.errorLabel.TabIndex = 6;
+            this.errorLabel.Text = ".";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(74, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(92, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Champion";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.textBox1.Location = new System.Drawing.Point(74, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Yasuo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(141, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select your champion and Save!";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.checkBox1.Location = new System.Drawing.Point(383, 261);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Auto Accept";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(12, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "More features soon!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(266, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ban Champion";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Crimson;
+            this.textBox2.Location = new System.Drawing.Point(260, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "Zed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(495, 325);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.open);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Instalock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label open;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,366 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Locker
+{
+    // Code & Docs by Bogdan (github.com/bogyz123)
+    public partial class Form1 : Form
+    {
+        public Timer clientTimer;
+        public Timer championTimer;
+        public string port;
+        public HttpClient client;
+        public string auth;
+        public string baseUrl;
+        public Form1()
+        {
+            InitializeComponent();
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            errorLabel.ForeColor = System.Drawing.Color.Red;
+            client = new HttpClient();
+            clientTimer = new Timer();
+            championTimer = new Timer();
+            clientTimer.Interval = 200;
+            clientTimer.Tick += ClientTimer_Tick;
+            championTimer.Interval = 500;
+            championTimer.Tick += ChampionTimer_Tick;
+
+        }
+
+        private async void ChampionTimer_Tick(object sender, EventArgs e)
+        {
+            GameState state = await GetGameStateAsync();
+            if (state == GameState.ReadyCheck)
+            {
+                if (checkBox1.Checked)
+                {
+                    await AcceptMatch();
+                }
+            }
+            else if (state == GameState.ChampSelect)
+            {
+
+                dynamic actionId = await GetActionId();
+                if (actionId != null)
+                {
+                    string actionType = actionId["actionType"];
+                    string id = actionId["id"];
+                    if (actionType == "pick")
+                    {
+                        string championToPick = GetChampionIdByName(textBox1.Text);
+                        await PickChamp(id, championToPick, "pick");
+                    }
+                    else if (actionType == "ban")
+                    {
+                        string championToBan = GetChampionIdByName(textBox2.Text);
+                        await PickChamp(id, championToBan, "ban");
+
+                    }
+                }
+            }
+
+
+
+        }
+        private async Task<Dictionary<string, string>> GetActionId()
+        {
+            string ourSummonerId = await GetSummonerIdAsync();
+
+            string endpoint = baseUrl + "lol-champ-select/v1/session";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            request.Headers.Add("Authorization", "Basic " + auth);
+
+            var data = await client.SendAsync(request);
+            var response = await data.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(response);
+
+            foreach (dynamic player in json.myTeam)
+            {
+                if (player.summonerId == ourSummonerId)
+                {
+                    var ourCellId = player.cellId;
+                    foreach (JArray action in json.actions)
+                    {
+                        for (int i = 0; i < action.Count; i++)
+                        {
+                            dynamic act = (dynamic)action[i];
+                            if (act.actorCellId == ourCellId)
+                            {
+                                if ((bool)act.isInProgress)
+                                {
+                                    Dictionary<string, string> info = new Dictionary<string, string>();
+                                    info.Add("id", act["id"].ToString());
+                                    info.Add("actionType", (string)act.type);
+                                    return info;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        class ChampionData
+        {
+            public string championId { get; set; }
+            public bool completed { get; set; }
+        }
+        private async Task PickChamp(string actionId, string championId, string actionType)
+        {
+            string declareIntent = baseUrl + $"lol-champ-select/v1/session/actions/{actionId}";
+            string lockIntent = baseUrl + $"lol-champ-select/v1/session/actions/{actionId}/complete";
+
+
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(new HttpMethod("PATCH"), declareIntent);
+            requestMessage.Headers.Add("Authorization", "Basic " + auth);
+            var content = new
+            {
+                championId = championId
+            };
+            string toJson = JsonConvert.SerializeObject(content);
+            var stringContent = new StringContent(toJson, Encoding.UTF8, "application/json");
+            requestMessage.Content = stringContent;
+            await client.SendAsync(requestMessage);
+            // Lock in champion
+            var lockRequest = new HttpRequestMessage(HttpMethod.Post, lockIntent);
+            lockRequest.Headers.Add("Authorization", "Basic " + auth);
+            var lockContent = new ChampionData
+            {
+                championId = championId,
+                completed = true
+            };
+
+            string lockContentJson = JsonConvert.SerializeObject(lockContent);
+            var stringContentJson = new StringContent(lockContentJson, Encoding.UTF8, "application/json");
+            lockRequest.Content = stringContentJson;
+            await client.SendAsync(lockRequest);
+
+
+        }
+
+
+
+
+        private async Task<string> GetSummonerIdAsync()
+        {
+            string endpoint = baseUrl + "lol-summoner/v1/current-summoner";
+            var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            request.Headers.Add("Authorization", "Basic " + auth);
+            var response = await client.SendAsync(request);
+            var message = await response.Content.ReadAsStringAsync();
+            dynamic asJson = JsonConvert.DeserializeObject(message);
+            return asJson.summonerId.ToString();
+        }
+        class Champion
+        {
+            public string key { get; set; }
+            public string id { get; set; }
+        }
+        private string GetChampionIdByName(string championName)
+        {
+            var data = GetChampionIds();
+            foreach (var item in data)
+            {
+                if (item.Key == championName)
+                {
+                    return item.Value.ToString();
+                }
+            }
+            return null;
+        }
+        private Dictionary<string, string> GetChampionIds()
+        {
+            string championDataUrl = "http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json";
+            Dictionary<string, string> championDictionary = new Dictionary<string, string>();
+
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    string json = wc.DownloadString(championDataUrl);
+
+                    JObject championData = JObject.Parse(json)["data"] as JObject;
+
+                    foreach (JProperty championProperty in championData.Children())
+                    {
+                        Champion champion = championProperty.Value.ToObject<Champion>();
+                        championDictionary[champion.id] = champion.key;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            return championDictionary;
+        }
+        private async Task AcceptMatch()
+        {
+            string endpoint = baseUrl + "lol-matchmaking/v1/ready-check/accept";
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("POST"), endpoint);
+            request.Headers.Add("Authorization", "Basic " + auth);
+            await client.SendAsync(request);
+        }
+
+        enum GameState
+        {
+            ReadyCheck,
+            ChampSelect,
+            Lobby,
+            Error,
+            None
+        }
+
+        private void ClientTimer_Tick1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+        private async Task<GameState> GetGameStateAsync()
+        {
+            string endpoint = baseUrl + "lol-gameflow/v1/gameflow-phase";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            request.Headers.Add("Authorization", "Basic " + auth);
+            try
+            {
+                var res = await client.SendAsync(request);
+                string state = await res.Content.ReadAsStringAsync();
+                string trimmed = state.Trim('"');
+                switch (trimmed)
+                {
+                    case "Lobby":
+                        {
+                            return GameState.Lobby;
+                        }
+                    case "ReadyCheck":
+                        {
+                            return GameState.ReadyCheck;
+                        }
+                    case "ChampSelect":
+                        {
+                            return GameState.ChampSelect;
+                        }
+                }
+            }
+            catch (Exception ex) { }
+            return GameState.Error;
+        }
+
+        private async void ClientTimer_Tick(object sender, EventArgs e)
+        {
+            var process = IsClientRunning();
+            if (process != null)
+            {
+                process.Exited += Process_Exited;
+                process.EnableRaisingEvents = true;
+                open.ForeColor = System.Drawing.Color.Green;
+                open.Text = "LeagueClient is running!";
+                var data = await GetConnectionDetailsAsync();
+                if (data != null)
+                {
+                    var port = data.Item1;
+                    var connection = data.Item2;
+                    auth = Convert.ToBase64String(Encoding.ASCII.GetBytes("riot:" + connection));
+                    baseUrl = "https://127.0.0.1:" + port + "/";
+
+                }
+                panel1.Visible = true;
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var process = IsClientRunning();
+            if (process != null)
+            {
+                open.ForeColor = System.Drawing.Color.Green;
+                open.Text = "LeagueClient is running!";
+                var data = await GetConnectionDetailsAsync();
+                if (data != null)
+                {
+                    var port = data.Item1;
+                    var connection = data.Item2;
+                    auth = Convert.ToBase64String(Encoding.ASCII.GetBytes("riot:" + connection));
+                    baseUrl = "https://127.0.0.1:" + port + "/";
+                }
+                process.Exited += Process_Exited;
+                process.EnableRaisingEvents = true;
+                panel1.Visible = true;
+            }
+            else
+            {
+                open.ForeColor = System.Drawing.Color.Red;
+                open.Text = "LeagueClient is closed!";
+                panel1.Visible = false;
+                clientTimer.Start();
+            }
+        }
+        private async Task<Tuple<string, string>> GetConnectionDetailsAsync()
+        {
+            string path = @"C:\Riot Games\League of Legends\lockfile";
+
+            if (File.Exists(path))
+            {
+                using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (StreamReader rdr = new StreamReader(stream))
+                    {
+                        var data = await rdr.ReadToEndAsync();
+                        var parts = data.Split(':');
+
+                        if (parts.Length >= 4)
+                        {
+                            var port = parts[2];
+                            var connection = parts[3];
+                            return new Tuple<string, string>(port, connection);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            panel1.Invoke((MethodInvoker)delegate
+            {
+                open.ForeColor = System.Drawing.Color.Red;
+                open.Text = "LeagueClient is closed!";
+                panel1.Visible = false;
+                clientTimer.Start();
+            });
+        }
+        private Process IsClientRunning()
+        {
+            Process[] leagueProcesses = Process.GetProcessesByName("LeagueClient");
+            if (leagueProcesses.Length > 0)
+            {
+                return leagueProcesses[0];
+            }
+            return null; // Return null when the process is not running
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                errorLabel.Text = "Error, enter a champion.";
+                return;
+            }
+            championTimer.Start();
+        }
+    }
+}
